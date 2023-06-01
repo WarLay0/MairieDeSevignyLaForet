647cbe6b05bedbe5b1f0e66798464a3c6516178b
unknown <evan.bombart.pro@gmail.com>
2023-06-01 16:01:25 -0700

    sauvegarde

--- admin/adminAssets/crud/eventAdd.php
+++ admin/adminAssets/crud/eventAdd.php
@@ -2,9 +2,9 @@
     <label for="title">Titre</label>
     <input type="text" name="title" id="title">
     <label for="start">Début</label>
-    <input type="datetime-local" name="start" id="start">
+    <input type="date" name="start" id="start">
     <label for="end">Fin</label>
-    <input type="datetime-local" name="end" id="end">
+    <input type="date" name="end" id="end">
     <label for="url">Lien</label>
     <input type="text" name="url" id="url">
     <input type="submit" value="Ajouter">
