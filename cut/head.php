ffeed755eeb9f88dbecab5d42efe464cedac3432
unknown <evan.bombart.pro@gmail.com>
2023-06-20 11:05:29 +0200

    :rocket: | Ajout de la balise google analytics

--- cut/head.php
+++ cut/head.php
@@ -20,6 +20,17 @@
     <!-- FullCalendar -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+
+    
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-G1LJ83H2B8"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-G1LJ83H2B8');
+    </script>
 </head>
 <body>
     <?php
